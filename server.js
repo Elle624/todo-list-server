d7fcb77620491d3e5eecabbe3d4e0fbc444aff3e
Yiran Li <yiran.li@hotmail.com>
2020-12-26 21:19:50 -0700

    Import todoList file and add Get endpoint

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { todoList as todoListDeets } from './todo-list.js';
 const app = express();
 
 app.set('port', process.env.PORT || 3001);
@@ -7,9 +8,14 @@ app.use(express.json());
 app.use(cors());
 
 app.locals.title = 'Todo List';
+app.locals.todoList = { todoList: todoListDeets };
 
 app.listen(app.get('port'), () => {
   console.log(
     `${app.locals.title} is running on http://localhost:${app.get('port')}.`
   );
+});
+
+app.get('/todo-list', (request, response) => {
+  response.json(app.locals.todoList);
 });
