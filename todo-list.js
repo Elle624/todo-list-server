fb38aaf88d11b75d42a2601f26c88a73ec717877
Yiran Li <yiran.li@hotmail.com>
2020-12-26 21:09:48 -0700

    Create toDo list file

---
+++ todo-list.js
@@ -0,0 +1,9 @@
+export const todoList = [
+  { text: 'Feed Isabel, Felix, and Amber', complete: true },
+  { text: 'Clean up carpet', complete: false },
+  { text: 'Go for a walk', complete: true },
+  { text: 'Get a haircut', complete: false },
+  { text: 'Get a car wash', complete: true },
+  { text: 'Buy groceries', complete: false },
+  { text: 'Watch a movie', complete: true },
+];
